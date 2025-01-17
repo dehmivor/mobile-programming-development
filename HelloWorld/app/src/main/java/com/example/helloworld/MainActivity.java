88f2e88af3c83deb5ed45c2bf542f1f6aa0fced6
minhtam <minhtamnhgp03@gmail.com>
2025-01-17 09:38:14 +0700

    layout facebook + ưu tiên top left + đúng từng chi tiết

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,31 +1,27 @@
 package com.example.helloworld;
 
-import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-        setContentView(R.layout.exercise2);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
 
-//        TextView tv_price = findViewById(R.id.tv_price);
-//        tv_price.setTextColor(Color.BLUE);
-//        tv_price.setTextColor(getColor(R.color.yellow));
-//        tv_price.setText(R.string.gi_250000);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Thanh Status Bar
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, android.R.color.black));
+
+            // Thanh Navigation Bar
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, android.R.color.black));
+        }
+
+
+        // Set layout
+        setContentView(R.layout.layout_facebook);
     }
 }
